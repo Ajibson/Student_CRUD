a0ebea91ff23bca32db551cdb6db6c057da10d9d
Ajibson <azeezajibson123@gmail.com>
2021-07-31 21:18:53 +0100

    Update settings.py
    static files setting added

--- my_first_models/settings.py
+++ my_first_models/settings.py
@@ -112,8 +112,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+#Add the lines to settings.py file
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_ROOT_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
